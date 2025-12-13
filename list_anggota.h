8a0d4488a9c8cbe5bc057a76f00110638d777af7
radit1107 <raditfawwazgayatra@gmail.com>
2025-12-13 09:17:15 +0700

    list_anggota.h 80%

--- list_anggota.h
+++ list_anggota.h
@@ -5,31 +5,49 @@
 #include <string>
 using namespace std;
 
+/* Forward declaration */
 struct Peminjaman;
 
+/* ===== RELASI (TIDAK DIGUNAKAN DI TIPE A, DISIMPAN NULL) ===== */
 struct RelasiAnggota {
     Peminjaman *child;
     RelasiAnggota *next;
 };
 
+/* ===================== NODE ANGGOTA ===================== */
 struct Anggota {
     string idAnggota;
     string nama;
 
-    RelasiAnggota *relasi;
+    RelasiAnggota *relasi; // tidak digunakan (MLL Tipe A)
     Anggota *next;
     Anggota *prev;
 };
 
+/* ===================== LIST ANGGOTA (DLL) ===================== */
 struct ListAnggota {
     Anggota *first;
     Anggota *last;
 };
 
+/* ===================== PRIMITIF LIST ===================== */
 void createList(ListAnggota &L);
 Anggota* alokasiAnggota(string id, string nama);
+
+/* ===================== INSERT ===================== */
+void insertFirst(ListAnggota &L, Anggota* p);
 void insertLast(ListAnggota &L, Anggota* p);
+
+/* ===================== DELETE ===================== */
+void deleteFirst(ListAnggota &L);
+void deleteLast(ListAnggota &L);
+void deleteByID(ListAnggota &L, string id);
+
+/* ===================== FIND ===================== */
 Anggota* findAnggota(ListAnggota L, string id);
+
+/* ===================== PRINT ===================== */
 void printInfo(ListAnggota L);
+void printReverse(ListAnggota L);
 
 #endif
