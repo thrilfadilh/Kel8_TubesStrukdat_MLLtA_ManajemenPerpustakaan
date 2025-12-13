9701050837e2277ab731a0a52db05e4b365228aa
FarishaHarun <farishaharun@gmail.com>
2025-12-13 09:27:47 +0700

    Progres 80%

--- list_peminjaman.h
+++ list_peminjaman.h
@@ -7,18 +7,40 @@ using namespace std;
 
 struct Peminjaman {
     string idPeminjaman;
-    string tanggal;
-    Peminjaman *next;
+    string tanggalPinjam;
+    string tanggalKembali;
+    float denda;
 };
+
+struct elmPeminjaman {
+    Peminjaman info;
+    elmPeminjaman *next;
+};
+
+typedef elmPeminjaman* adrPeminjaman;
 
 struct ListPeminjaman {
-    Peminjaman *first;
+    adrPeminjaman first;
 };
 
+struct Buku;
+struct Anggota;
+
 void createList(ListPeminjaman &L);
-Peminjaman* alokasiPeminjaman(string id, string tanggal);
-void insertLast(ListPeminjaman &L, Peminjaman* p);
-Peminjaman* findPeminjaman(ListPeminjaman L, string id);
-void printInfo(ListPeminjaman L);
+adrPeminjaman alokasiPinjam(Peminjaman x);
+void insertLastPeminjaman(ListPeminjaman &L, adrPeminjaman p);
+void deletePeminjaman(ListPeminjaman &LP, adrPeminjaman p);
+adrPeminjaman findPinjam(ListPeminjaman L, string idPeminjaman);
+void showAllPeminjaman(ListPeminjaman L);
+
+void showChildByAnggota(ListPeminjaman L, Anggota* parent);
+void showChildByBuku(ListPeminjaman L, Buku* parent);
+void showAllAnggotaWithChild(ListPeminjaman L, ListAnggota LA);
+void showAllBukuWithChild(ListPeminjaman L, ListBuku LB);
+void showChildWithParents(ListPeminjaman L);
+void showParentsOfChild(ListPeminjaman L, string idPeminjaman);
+
+void tambahRelasiBuku(Buku *parent, adrPeminjaman child);
+void tambahRelasiAnggota(Anggota *parent, adrPeminjaman child);
 
 #endif
